c486ab541718cc8c93c08c1a8b25472139c70555
Sriram.B <sriram.b@trikatechnologies.com>
2022-12-20 13:19:14 +0530

    Implemented getting all candidates by their name

--- PursuitAPI/models/candidateModel.js
+++ PursuitAPI/models/candidateModel.js
@@ -6,7 +6,7 @@ const candidateSchema = new Schema(
   {
     firstName: { type: String, default: null },
     lastName: { type: String, default: null },
-    email: { type: String, default: null },
+    email: { type: String, required: true, default: null },
     phoneNumber: { type: String, default: null },
     gender: { type: String, required: true },
     dob: { type: String, required: true, default: null },
@@ -21,6 +21,7 @@ const candidateSchema = new Schema(
     referralEmail: { type: String, default: null },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
     createdDateTime: { type: Number, default: new Date().getTime() },
+    resumeUrl: { type: String, default: null },
   },
   {
     versionKey: false,
@@ -33,32 +34,85 @@ function model() {
   return candidateModel;
 }
 
-
 function addCandidateProfile(params) {
-    return new Promise((resolve,reject) => {
-      candidateModel.findOne({
-          email: params.email,
-        }).then((data) => {
-          if (!data) {
-            candidateModel.create(params)
-              .then((candidateData) => {
-                resolve(candidateData);
-              })
-              .catch((error) => {
-                reject(error);
+  return new Promise((resolve, reject) => {
+    candidateModel
+      .findOne({
+        email: params.email,
+      })
+      .then((data) => {
+        if (!data) {
+          candidateModel
+            .create(params)
+            .then((candidateData) => {
+              resolve(candidateData);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          reject("The candidate is already added to Pursuit");
+        }
+      })
+      .catch((error1) => {
+        reject(error1);
+      });
+  });
+}
 
-              });
-          } else {
-            reject("The candidate is already added to Pursuit");
-          }
-        })
-        .catch((error1) => {
-          reject(error1);
-        });
-    });
+function getCandidatesByName(params, callback) {
+  let searchText = params.searchText;
+  let limit = parseInt(params.limit);
+  let skip = parseInt(params.skip);
+  
+  let match;
+  if (searchText != "{searchText}" && searchText.length > 0) {
+    match = {
+      $or: [
+        { firstName: { $regex: searchText, $options: "i" } },
+        { lastName: { $regex: searchText, $options: "i" } },
+      ],
+      isDeleted: false,
+    };
+  } else {
+    match = {
+      isDeleted: false,
+    };
   }
+
+  let aggregateQuery = [
+    {
+      $match: match,
+    },
+    {
+      $project: {
+        firstName: 1,
+        lastName: 1,
+        email: 1,
+        phoneNumber: 1,
+        gender: 1,
+        dob: 1,
+        address: 1,
+        city: 1,
+        state: 1,
+        pincode: 1,
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+  ];
+
+  model().aggregate(aggregateQuery, function (error, response) {
+    callback(error, response);
+  });
+}
 
 module.exports = {
   model,
-  addCandidateProfile
+  addCandidateProfile,
+  getCandidatesByName,
 };
--- PursuitAPI/routes/candidates.js
+++ PursuitAPI/routes/candidates.js
@@ -63,15 +63,16 @@ let verifyToken = require("./../common/verifyToken");
 
 router.post("/addCandidateProfile", verifyToken, async function (req, res) {
   try {
-   
     await candidateModel
-      .addCandidateProfile(req.body).then((data1) => {
+      .addCandidateProfile(req.body)
+      .then((data1) => {
         let result = {
-            data : data1,
+          data: data1,
           message: "Candidates added successfully",
         };
         res.status(200).json(result);
-      }).catch((error) => {
+      })
+      .catch((error) => {
         let data = {
           message: error,
         };
@@ -84,6 +85,80 @@ router.post("/addCandidateProfile", verifyToken, async function (req, res) {
       message: e.message,
     };
     res.status(403).json(data);
+  }
+});
+
+
+/**
+ * @swagger
+ * /candidates/getCandidatesByName/{searchText}:
+ *   get:
+ *     summary: Get all the panelists by their name. 
+ *     tags:
+ *       - Candidates
+ *     description: Get all user profile details (Recruiters and Panelists) by their name as search parameter
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: x-access-token
+ *         description: send valid token
+ *         type: string
+ *         in: header
+ *       - name: searchText
+ *         description: searchText
+ *         type: string
+ *         in: query
+ *       - name: skip
+ *         description: skip default is 0
+ *         in: query
+ *         type: number
+ *         required: true
+ *       - name: limit
+ *         description: limit
+ *         in: query
+ *         type: number
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: To get panelists profile details
+ */
+
+router.get("/getCandidatesByName/:searchText", function (req, res) {
+  try {
+    let skip = req.query.skip;
+    let limit = req.query.limit;
+    let searchText = req.query.searchText ? req.query.searchText : "";
+  
+    let params = {
+      skip: skip,
+      limit: limit,
+      searchText: searchText,
+    };
+
+    candidateModel.getCandidatesByName(params, function (error, response) {
+      if (error) {
+        res.status(403).json({
+          message: error,
+        });
+      } else if (response.length == 0) {
+        res.status(403).json({
+          success: false,
+          message: "There is no candidates in the registry",
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          data: response,
+        });
+      }
+    });
+  } catch (e) {
+    let result = {
+      success: false,
+      message: e.message,
+      data: e,
+    };
+    res.status(403).json(result);
   }
 });
 
